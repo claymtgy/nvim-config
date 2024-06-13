0a2c136bc3f8c2b7a4fdeeb1cae45b5b06ca2f50
claymtgy <claymtgy@gmail.com>
2024-06-13 16:49:54 -0600

    add composer to installation of dependencies

--- installation.sh
+++ installation.sh
@@ -23,7 +23,7 @@ fi
 if [[ $choice == 3 ]]; then
   echo "Sudo needed to install dependencies"
   sudo apt update
-  sudo apt-get install -y neovim golang-go npm python3 python3-pip python3-venv python3-dev python3-pynvim
+  sudo apt-get install -y neovim golang-go npm python3 python3-pip python3-venv python3-dev python3-pynvim composer
   echo "Finished installing dependencies"
 fi
 
